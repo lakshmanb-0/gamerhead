a1c2bb67c34542a11bb8741201d3327a8edb2703
Lakshman <lakshmanram2259@gmail.com>
2024-03-22 11:35:41 +0530

    Update apiCalls.ts

--- app/server.ts/apiCalls.ts
+++ app/server.ts/apiCalls.ts
@@ -1,35 +1,61 @@
 'use server'
 
-const fetchData = async (api: string) => {
-    return await fetch(api)
+export const getCategory = async () => {
+    const result = await fetch(`http://store.steampowered.com/api/featuredcategories/?cc=IN&l=english&json=1`)
         .then(res => res.json())
         .then(data => data)
         .catch(error => console.log(error))
-}
-export const getCategory = async () => {
-    return await fetchData(`http://store.steampowered.com/api/featuredcategories/?cc=IN&l=english&json=1`)
+    return result;
 }
 export const getFeature = async () => {
-    return await fetchData(`https://store.steampowered.com/api/featured/?cc=IN&l=english&json=1`)
+    const result = await fetch(`https://store.steampowered.com/api/featured/?cc=IN&l=english&json=1`)
+        .then(result => result.json())
+        .then(data => data)
+        .catch(error => console.log(error))
+    return result;
 }
 export const getTopReleases = async () => {
-    return await fetchData(`https://api.steampowered.com/ISteamChartsService/GetTopReleasesPages/v1/?key=${process.env.REACT_APP_STEAM_KEY}&cc=IN&l=english&json=1`)
+    const result = await fetch(`https://api.steampowered.com/ISteamChartsService/GetTopReleasesPages/v1/?key=${process.env.REACT_APP_STEAM_KEY}&cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(error => console.log(error))
+    return result
 }
 export const getDlc = async (params: number) => {
-    return await fetchData(`https://store.steampowered.com/api/dlcforapp/?appid=${params}&cc=IN&l=english&json=1`)
+    const result = await fetch(`https://store.steampowered.com/api/dlcforapp/?appid=${params}&cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(error => console.log(error))
+    return result
 }
 export const getAppDetails = async (params: number) => {
-    return await fetchData(`http://store.steampowered.com/api/appdetails/?appids=${params}&cc=IN&l=english&json=1`)
+    const result = await fetch(`http://store.steampowered.com/api/appdetails/?appids=${params}&cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(error => console.log(error))
+    return result
 }
 export const getPlayer = async (params: string) => {
-    return await fetchData(`https://steamcommunity.com/actions/ajaxresolveusers?steamids=${params}&cc=IN&l=english&json=1`)
+    const result = await fetch(`https://steamcommunity.com/actions/ajaxresolveusers?steamids=${params}&cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .catch(error => console.log(error))
+    return result
 }
 export const getNews = async (params: number) => {
-    return await fetchData(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?key=${process.env.REACT_APP_STEAM_KEY}&appid=${params}&cc=IN&l=english&json=1`)
+    const result = await fetch(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?key=${process.env.REACT_APP_STEAM_KEY}&appid=${params}&cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .catch(error => console.log(error))
+    return result
 }
 export const getReviews = async (params: number) => {
-    return await fetchData(`https://store.steampowered.com/appreviews/${params}?cc=IN&l=english&json=1`)
+    const result = await fetch(`https://store.steampowered.com/appreviews/${params}?cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .catch(error => console.log(error))
+    return result
 }
 export const getSearch = async (params: string) => {
-    return await fetchData(`https://steamcommunity.com/actions/SearchApps/${params}?cc=IN&l=english&json=1`)
+    const result = await fetch(`https://steamcommunity.com/actions/SearchApps/${params}?cc=IN&l=english&json=1`)
+        .then(res => res.json())
+        .catch(error => console.log(error))
+    return result
 }
